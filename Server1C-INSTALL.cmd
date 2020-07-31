2e16081420ffdb12f90ab81769099dc434ba9345
Федор Кубанец <f.kubanets@happylook.ru>
2020-07-31 11:38:02 +0300

    Исправил создание сервиса

--- Server1C-INSTALL.cmd
+++ Server1C-INSTALL.cmd
@@ -14,7 +14,8 @@ icacls %SERVERDIR% /grant %SERVERLOGIN%:(OI)(CI)(M) /inheritance:e
 IF NOT EXIST "%ProgramFiles%"\1cv8\current mklink /J "%ProgramFiles%"\1cv8\current "%ProgramFiles%"\1cv8\%NEWVERSION%
 rem "%ProgramFiles%"\1cv8\current\bin\ragent.exe -instsrvc -d %SERVERDIR% -debug -usr %SERVERLOGIN% -pwd %SERVERPASSWORD%
 mklink /J "%ProgramFiles%"\1cv8\current "%ProgramFiles%"\1cv8\%NEWVERSION%
-sc create %SERVICENAME% start= auto binPath= "\"%ProgramFiles%\1cv8\current\bin\ragent.exe\" -d %SERVERDIR% -debug -srvc -agent -port %PORTPREFIX%40 -regport %PORTPREFIX%41 -range %PORTPREFIX%60:%PORTPREFIX%91" DisplayName= %SERVICEDESCR% depend= Tcpip/Dnscache/lanmanworkstation/lanmanserver
+sc create %SERVICENAME% binPath= "\"%ProgramFiles%\1cv8\current\bin\ragent.exe\" -d %SERVERDIR:"=\"% -debug -srvc -agent -port %PORTPREFIX%40 -regport %PORTPREFIX%41 -range %PORTPREFIX%60:%PORTPREFIX%91"
+sc config %SERVICENAME% DisplayName= %SERVICEDESCR% Start= auto depend= "Tcpip/Dnscache/lanmanworkstation/lanmanserver"
 sc config %SERVICENAME% obj= %SERVERLOGIN% password= %SERVERPASSWORD%
 sc description %SERVICENAME% %SERVICEDESCR%
 sc failure %SERVICENAME% reset= INFINITE actions= restart/10000
